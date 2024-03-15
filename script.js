a62b9d80eebc19379b292c62a5c90e8f212ee804
LuisMarioB5 <37335260+LuisMarioB5@users.noreply.github.com>
2024-03-14 20:26:32 -0400

    Actualización del JS.

--- script.js
+++ script.js
@@ -1,13 +1,28 @@
 // Variable para almacenar el idioma actual de la página
 var idioma;
 
+// Definición de los elementos HTML, utilizando el DOM
+var logo;
+var h1;
+var botones_idioma;
+var texto_entrada;
+var img_precaucion;
+var alerta;
+var encriptar;
+var desencriptar;
+var texto_salida;
+var copiar;
+var img_error;
+var titulo_error;
+var parrafo_error;
+
 // Este evento se dispará luego de que todo el contenido HTML este completamente cargado en la página
 document.addEventListener('DOMContentLoaded', function() {
     // Selecciona el primer botón con la clase 'idioma'
     let idioma_predeterminado = document.querySelector('.idioma');
 
     // Llama a la función cambiar_idioma para seleccionar el idioma del primer botón
-    cambiar_idioma(idioma_predeterminado); 
+    cambiar_idioma(idioma_predeterminado);
 });
 
 // Función para cambiar el idioma de los elementos de la página
@@ -15,20 +30,19 @@ function cambiar_idioma(boton_presionado) {
     // Transformando el valor del boton a minúsculas (lowercase)
     idioma = boton_presionado.textContent.toLowerCase();
 
-    // Definición de los elementos HTML, utilizando el DOM
-    let logo = document.getElementById('logo');
-    let h1 = document.getElementById('titulo-principal');
-    let botones_idioma = document.querySelectorAll('.idioma');
-    let texto_entrada = document.getElementById('texto-entrada');
-    let img_precaucion = document.getElementById('img-precaucion');
-    let alerta = document.getElementById('precaucion-texto');
-    let encriptar = document.getElementById('boton-encriptar');
-    let desencriptar = document.getElementById('boton-desencriptar');
-    let texto_salida = document.getElementById('texto-salida');
-    let copiar = document.getElementById('boton-copiar');
-    let img_error = document.getElementById('img-error');
-    let titulo_error = document.getElementById('titulo-error')
-    let parrafo_error = document.getElementById('parrafo-error')
+    logo = document.getElementById('logo');
+    h1 = document.getElementById('titulo-principal');
+    botones_idioma = document.querySelectorAll('.idioma');
+    texto_entrada = document.getElementById('texto-entrada');
+    img_precaucion = document.getElementById('img-precaucion');
+    alerta = document.getElementById('precaucion-texto');
+    encriptar = document.getElementById('boton-encriptar');
+    desencriptar = document.getElementById('boton-desencriptar');
+    texto_salida = document.getElementById('texto-salida');
+    copiar = document.getElementById('boton-copiar');
+    img_error = document.getElementById('img-error');
+    titulo_error = document.getElementById('titulo-error')
+    parrafo_error = document.getElementById('parrafo-error')
 
     // Vacia la clase 'presionado' para que solo exista un botón en esta clase
     botones_idioma.forEach(function(boton) {
@@ -48,7 +62,13 @@ function cambiar_idioma(boton_presionado) {
         alerta.textContent = 'Solo letras minúsculas y sin acentos';
         encriptar.textContent = 'Encriptar';
         desencriptar.textContent = 'Desencriptar';
-        copiar.textContent = 'Copiar';
+        if (copiar.textContent === 'Copied Text!') {
+            copiar.textContent = 'Texto Copiado!';
+        }
+        else {
+            copiar.textContent = 'Copiar';
+        }
+        
         texto_salida.placeholder = 'Visualización del resultado';
         img_error.alt = 'Imagen de una mujer tratando de encontrar un diamante.';
         titulo_error.textContent = 'Ningún mensaje fue encontrado';
@@ -65,7 +85,14 @@ function cambiar_idioma(boton_presionado) {
         alerta.textContent = 'Only lowercase letters and no accents';
         encriptar.textContent = 'Encrypt';
         desencriptar.textContent = 'Decrypt';
-        copiar.textContent = 'Copy';
+
+        if (copiar.textContent === 'Texto Copiado!') {
+            copiar.textContent = 'Copied Text!';
+        }
+        else {
+            copiar.textContent = 'Copy';
+        }
+
         texto_salida.placeholder = 'Result display';
         img_error.alt = 'Image of a woman trying to find a diamond.';
         titulo_error.textContent = 'No message was found';
@@ -93,4 +120,14 @@ function copiar_portapapeles() {
     }
 }
 
+/* ADAPTAR PARA LA ENCRIPTACION */
+function encriptar_mensaje() {
+    texto_salida.textContent = texto_entrada.textContent;
 
+    if (idioma === 'es') {
+        copiar.textContent = 'Si Señor!!!';
+    }
+    else if (idioma === 'en') {
+        copiar.textContent = 'Yessir!!!';
+    }
+}
